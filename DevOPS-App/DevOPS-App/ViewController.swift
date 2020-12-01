5017ac5a4522f51e45929254b02e884d4a6c36e9
Admin <xvilakaz@student.wethinkcode.co.za>
2020-12-01 16:42:44 +0200

    Testing CI/CD tools

---
+++ DevOPS-App/DevOPS-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevOPS-App
+//
+//  Created by Admin on 2020/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
